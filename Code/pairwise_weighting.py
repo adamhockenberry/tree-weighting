fcac9b99095d7ffc635ec49976f013393a05820e
Adam Hockenberry <hockenberry.adam@gmail.com>
2019-03-21 15:53:05 -0500

    Did a substantial commenting/editing session on these methods

--- Code/pairwise_weighting.py
+++ Code/pairwise_weighting.py
@@ -2,57 +2,94 @@ import numpy as np
 
 
 def get_weight_matrices(my_tree):
+    """
+    This is code that implements a method I developed to get weights for pairwise comparisons
+    that is similar in ideology to the GSC (Gerstein, Sonnhammer, Chothia) algorithm for weighting
+    individual nodes in a tree. 
+
+    Wouldn't be surprised if someone else came up with this idea at some point but I've yet to see
+    it.
+
+    Note that this may alter the original tree by first setting the root node to have a length of zero
+    and then making it equal to None.
+
+    Input(s):
+    my_tree - a Bio.Phylo tree object. No root required!
+
+    Output(s):
+    ending_weight_matrix - the matrix of weights for all terminal-terminal pairs (rows and columns)
+    ending_weight_matrix_normalized - ending_weight_matrix divided by the ending_matrix_raw 
+    ending_matrix_raw - the matrix of distances between all terminal-terminal pairs
+    my_tree.get_terminals() - list of terminals that are critical for knowing row/col entries in 
+            the matrices. This should be obvious but am returning it to be super sure everyone knows
+
+    """
     term_count = len(my_tree.get_terminals())
-    starting_matrix_weighted = np.zeros((term_count, term_count))
+    #Instantiate the matrices that I'll use throughout
+    starting_weight_matrix = np.zeros((term_count, term_count))
     starting_matrix_raw = np.zeros((term_count, term_count))
     my_tree.root.branch_length = 0.
     term_pairs = enumerate(my_tree.get_terminals())
     term_pairs = [(pair[1], pair[0]) for pair in term_pairs]
     term_dict = dict(term_pairs)
     
-    ending_matrix_weighted, ending_matrix_raw, finished\
+    ending_weight_matrix, ending_matrix_raw, finished\
             = pairwise_distances_recursive(my_tree.root, my_tree,\
-                                                              starting_matrix_weighted,\
+                                                              starting_weight_matrix,\
                                                               starting_matrix_raw,\
                                                               term_dict, [])
-    ending_matrix_normalized = np.divide(ending_matrix_weighted, ending_matrix_raw,\
-                                     out=np.zeros_like(ending_matrix_weighted), where=ending_matrix_raw!=0)
+    ending_weight_matrix_normalized = np.divide(ending_weight_matrix, ending_matrix_raw,\
+                                     out=np.zeros_like(ending_weight_matrix), where=ending_matrix_raw!=0)
     my_tree.root.branch_length = None
-    return ending_matrix_raw, ending_matrix_weighted, ending_matrix_normalized
+    return ending_weight_matrix, ending_weight_matrix_normalized, ending_matrix_raw, my_tree.get_terminals()
 
-def pairwise_distances_recursive(putative_root, my_tree, matrix_weighted, matrix_raw, term_map_dict, finished):
-    '''
-    my_tree is a Bio.Phylo tree
-    putative_root should probably be my_tree.root to start the process
-    matrix_weighted and matrix_raw are nxn np.zeros matrices where n is the number of terminals in the tree
-    term_map_dict speeds life up by indexing key:val pairs as: "terminal object":"index in matrices"
-    finished is just an empty list to start and tracks progress of the recursive algorithm
-
+def pairwise_distances_recursive(putative_root, my_tree, weight_matrix, matrix_raw, term_map_dict, finished):
+    """
     Side note, this took weeks, WEEKS, to get working and I'm very proud of it's overall simplicity despite the 
     handful of notes and the obvious copy/paste that shouldn't be there.
-    '''
+
+    Input(s):
+    putative_root - a Bio.Phylo clade object, should probably be my_tree.root to start the process
+    my_tree - the Bio.Phylo tree
+    weight_matrix and matrix_raw - start as n x n np.zeros matrices where n is the number of terminals 
+            in the tree. These matrices get values added to them throughout the recursion
+    term_map_dict - a dictionary that speeds things up by indexing key:val pairs 
+            as: "terminal object":"index in matrices"
+    finished - an empty list to start and tracks progress of the recursive algorithm
+    
+    Output(s):
+    weight_matrix - the matrix of weights for all terminal-terminal pairs (rows and columns)
+    matrix_raw - the matrix of distances between all terminal-terminal pairs
+    finished - record keeping
+
+    """
     ###Could speed things up by passing ds_terms and us_terms around a bit
     ds_terms = [term_map_dict[i] for i in putative_root.get_terminals()]
     ###Should make this a set addition problem at very least
     us_terms = [term_map_dict[i] for i in my_tree.get_terminals() if term_map_dict[i] not in ds_terms] 
+    
+    #This really just skips the root which should have no upstream terms and the branch length
+    #along the root node is contained in Bio.Phylo trees by the branch_length of the two descendants
     if min([len(ds_terms), len(us_terms)]) > 0:
         ###################
-        ###For the weighted matrix
+        ###For the weights matrix
         ###################
         val = putative_root.branch_length/(len(ds_terms)*len(us_terms))
         ###Start at zero
-        to_add = np.zeros(matrix_weighted.shape)
+        to_add = np.zeros(weight_matrix.shape)
         ###Add rows and column values
         to_add[len(finished):len(ds_terms)+len(finished), :] = val        
         to_add[:, len(finished):len(ds_terms)+len(finished)] = val
         ###Subtract center square
         to_add[len(finished):len(ds_terms)+len(finished), len(finished):len(ds_terms)+len(finished)] = 0
         ###Add to matrix
-        matrix_weighted += to_add
+        weight_matrix += to_add
         
         #####################
-        ###For the raw matrix (copy/paste of above, basically)
-        ###################
+        ###For the raw matrix
+        #####################
+        #Which is  to say, partitioning the branch_length to all straddling terminals
+        #without dividing it by the number of straddling terminals
         val = putative_root.branch_length
         ###Start at zero
         to_add = np.zeros(matrix_raw.shape)
@@ -65,15 +102,15 @@ def pairwise_distances_recursive(putative_root, my_tree, matrix_weighted, matrix
         matrix_raw += to_add
         
     if len(putative_root.clades) == 2:
-        matrix_weighted, matrix_raw, finished = pairwise_distances_recursive(putative_root.clades[0],\
-                                                                      my_tree, matrix_weighted, matrix_raw,\
+        weight_matrix, matrix_raw, finished = pairwise_distances_recursive(putative_root.clades[0],\
+                                                                      my_tree, weight_matrix, matrix_raw,\
                                                                       term_map_dict, finished)
-        matrix_weighted, matrix_raw, finished = pairwise_distances_recursive(putative_root.clades[1],\
-                                                                      my_tree, matrix_weighted, matrix_raw,\
+        weight_matrix, matrix_raw, finished = pairwise_distances_recursive(putative_root.clades[1],\
+                                                                      my_tree, weight_matrix, matrix_raw,\
                                                                       term_map_dict, finished)
     elif len(putative_root.clades) == 0:
         finished.append(putative_root)
-    return matrix_weighted, matrix_raw, finished
+    return weight_matrix, matrix_raw, finished
 
 
 
--- Code/weighting_methods.py
+++ Code/weighting_methods.py
@@ -5,134 +5,152 @@ import random
 
 from collections import Counter
 
-def normalize_GSC_weights(weights_dict, rooted_tree):
-    '''
-    Scratch function at the moment
+###################
+#ACL weights
+###################
+def GSC_adhock(rooted_tree, output_all=False):
+    """
+    This is a fairly straightforward and fast implementation of the Gerstein-Sonnhammer-Chothia
+    weighting algorithm (GSC). Am sure it could be simplified and probably sped up but it scales
+    well and works.
+    
+    NOTE: I saw a root-first algorithm in PyCogent that could definitely speed this up but would
+    have to consider how that algorithm would play with other downstream applications that use these
+    weights (specifically the full_weights_dict).
 
-    '''
-    normed_weights_dict = {}
-    depths_dict = rooted_tree.depths()
-    for term in rooted_tree.get_terminals():
-        normed_weights_dict[term] = [weights_dict[term][-1]/depths_dict[term]]
-    return normed_weights_dict
-
-def GSC_adhock_old(rooted_tree):
-    '''
-    This is a fairly straightforward and fast implementation of the GSC algorithm using a Bio.Phylo 
-    tree object as input. The output dictionary has values that are lists where the final list element
-    is the weight but it keeps track of the weight at each depth which is useful later.
-    '''
-    ###Initialize the weights dictionary with zeros
-    weights_dict = {}
+    Input(s):
+    rooted_tree - a Bio.Phylo tree object that should be properly rooted for the numbers to make any sense
+    output_all - a boolean that when toggled to True (or really any non-false value) will return two
+            dictionaries. The basic weights_dict described below and an optional full_weights_dict
+            that keeps track of the weight of each node at each depth and is useful for certain applications
+            that require repeated calculation of weights for various root points on a tree
+    
+    Output(s):
+    weights_dict - a dictionary object with key:val pairs that are Bio.Phylo clade objects (specifically 
+            terminals)(keys) and floats that represent the weight (vals)
+    full_weights_dict(optional) - a dictionary object with key:val pairs that are Bio.Phylo clade objects
+            (specifically terminals)(keys) and lists of the weight assigned at each depth (vals). The final
+            entry in the list is the correct weight assigned to each terminal. 
+    """
+    assert rooted_tree.rooted
+    assert rooted_tree.is_bifurcating()
+    
+    #Initialize the weights dictionary with zeros in the starting list for each terminal
+    full_weights_dict = {}
     for terminal in rooted_tree.get_terminals():
-        weights_dict[terminal] = [0.0]
+        full_weights_dict[terminal] = [0.0]
 
-    ###This can probably be cleaned up to just specify that the root shouldn't have any branch length
-    ###because I can't figure out why on earth it ever would
-    ###This code just gets the depths of each terminal
+    #This can probably be cleaned up to just specify that the root shouldn't have any branch length
+    #because I can't figure out why on earth it ever would
+    #This code just gets the depths of each terminal
     if rooted_tree.root.branch_length:
-        bl = rooted_tree.root.branch_length
-        all_depths_dict = rooted_tree.depths(unit_branch_lengths=True)
-        for key,val in all_depths_dict.items():
-            all_depths_dict[key] = int(val-bl)
-    else:
-        all_depths_dict = rooted_tree.depths(unit_branch_lengths=True)
-    ###Little trick here to alter the all_depths_dict but unclear whether it does anything at the moment. WTF
-    #all_depths_dict = {key: all_depths_dict[key] for key in rooted_tree.get_nonterminals()+rooted_tree.get_terminals()}
+        rooted_tree.root.branch_length = None
+        print("The root appeared to have branch length which was removed during the"
+                "calculation of GSC weights")
+    #Get the depths of all nodes in unit branch lengths
+    all_depths_dict = rooted_tree.depths(unit_branch_lengths=True)
+    #Convert this dictionary into a "reverse" dictionary where depth is the key
+    #and the value is a list of all nodes at that particular depth
     all_depths_reverse_dict = defaultdict(list)
     for key, val in all_depths_dict.items():
         all_depths_reverse_dict[val].append(key)
+    #####################
+    #On to the algorithm.
+    #####################
+    #Start with the deepest nodes and work backwards (i.e. leaf to root)
     for i in range(max(all_depths_reverse_dict.keys()), 0, -1):
-        for key,val in weights_dict.items():
-            weights_dict[key].append(val[-1])
+        #First copy over the previous value for each node
+        for key,val in full_weights_dict.items():
+            full_weights_dict[key].append(val[-1])
+        #Now iterate through the clades at that particular depth
         for clade in all_depths_reverse_dict[i]:
+            #Get all the downstream terminals
             try:
                 aterms = clade.clades[0].get_terminals()
                 bterms = clade.clades[1].get_terminals()
+            #This error will be raised for terminal nodes who subsequently get all
+            #their branch length without the need for any further calculations
             except IndexError:
-                weights_dict[clade][-1] += clade.branch_length
+                full_weights_dict[clade][-1] += clade.branch_length
                 continue
-            a = [weights_dict[term][-1] for term in aterms]
-            b = [weights_dict[term][-1] for term in bterms]
-            atot = np.sum(a)
-            btot = np.sum(b)
-            tot_weights = atot+btot
-            #I think this is a buggy implementation that produces the correct results for an example
-            #but makes less sense the more I think about it. The weight at each branch shouldn't be divided
-            #equally among all sequences in the left sub-tree for instances and should rather be divided
-            #to each sequence according to its weight
-            if tot_weights != 0:
-                afrac = (atot/tot_weights)/len(a)
-                bfrac = (btot/tot_weights)/len(b)
-            else:
-                afrac = 0.5/len(a)
-                bfrac = 0.5/len(b)
-            for term in aterms:
-                weights_dict[term][-1] += clade.branch_length*afrac
-            for term in bterms:
-                weights_dict[term][-1] += clade.branch_length*bfrac    
-    return weights_dict
-
-def GSC_adhock(rooted_tree):
-    '''
-    This modification just changes the way weights are partitioned. But I think it might have problems with leaves of
-    zero branch length so might not make as much sense as I initially thought
-    '''
-    ###Initialize the weights dictionary with zeros
-    weights_dict = {}
-    for terminal in rooted_tree.get_terminals():
-        weights_dict[terminal] = [0.0]
-
-    ###This can probably be cleaned up to just specify that the root shouldn't have any branch length
-    ###because I can't figure out why on earth it ever would
-    ###This code just gets the depths of each terminal
-    if rooted_tree.root.branch_length:
-        bl = rooted_tree.root.branch_length
-        all_depths_dict = rooted_tree.depths(unit_branch_lengths=True)
-        for key,val in all_depths_dict.items():
-            all_depths_dict[key] = int(val-bl)
-    else:
-        all_depths_dict = rooted_tree.depths(unit_branch_lengths=True)
-    all_depths_reverse_dict = defaultdict(list)
-    for key, val in all_depths_dict.items():
-        all_depths_reverse_dict[val].append(key)
-    for i in range(max(all_depths_reverse_dict.keys()), 0, -1):
-        for key,val in weights_dict.items():
-            weights_dict[key].append(val[-1])
-        for clade in all_depths_reverse_dict[i]:
-            try:
-                aterms = clade.clades[0].get_terminals()
-                bterms = clade.clades[1].get_terminals()
-            except IndexError:
-                weights_dict[clade][-1] += clade.branch_length
-                continue
-            a = [weights_dict[term][-1] for term in aterms]
-            b = [weights_dict[term][-1] for term in bterms]
+            #But for NON-terminal nodes, the branch length must be divided between 
+            #downstream terminals in a weghted manner
+            a = [full_weights_dict[term][-1] for term in aterms]
+            b = [full_weights_dict[term][-1] for term in bterms]
             tot_weights = np.sum(a+b)
             
             if tot_weights != 0:
+                #Divide the branch length according to the existing weight of each downstream terminal
                 for term in aterms + bterms:
-                    weights_dict[term][-1] += ((weights_dict[term][-1]/tot_weights)*clade.branch_length)
+                    full_weights_dict[term][-1] += ((full_weights_dict[term][-1]/tot_weights)*clade.branch_length)
+            #If the weight of the downstream terminals is all zero, divide the branch length evenly
+            #between them
             else:
                 for term in aterms + bterms:
-                    weights_dict[term][-1] += clade.branch_length/len(aterms+bterms)
-    return weights_dict
+                    full_weights_dict[term][-1] += clade.branch_length/len(aterms+bterms)
+    #Make a new dictionary of just the final weights
+    weights_dict = {}
+    for key, val in full_weights_dict.items():
+        weights_dict[key] = val[-1]
+    #Output both dictionaries if requested
+    if output_all:
+        return weights_dict, full_weights_dict
+    #Otherwise just return the basic weights_dict with the final values
+    else:
+        return weights_dict
+
+def normalize_GSC_weights(weights_dict, rooted_tree):
+    """
+    This is a novel (I believe) normalization that I came up with to re-scale GSC weights.
+    It simply divides the weight for each terminal by the overall depth of that terminal
+    and therefore can/should(?) be thought of as a % independence.
+
+    Input(s):
+    weights_dict - weights dictionary, output from "GSC_adhock"
+    rooted_tree - a Bio.Phylo tree object that was used as input for "GSC_adhock"
+    
+    Output(s):
+    normed_weights_dict - key:val pairs are Bio.Phylo clade objects (same as in the 
+            weights_dict) (keys) and a single number that represents the normalized weight.
+    """
+    normed_weights_dict = {}
+    depths_dict = rooted_tree.depths()
+    for term in rooted_tree.get_terminals():
+        normed_weights_dict[term] = [weights_dict[term]/depths_dict[term]]
+    return normed_weights_dict
+
 
 
 ###################
-#ACL weights#######
+#ACL weights
 ###################
-
-###NOTE: I'd like to think of a way to normalize these weights better
 def ACL_adhock(rooted_tree):
+    """
+    This is an implemntation of  Altschul, Carroll and Lipman weighting (ACL) that represents the current
+    flowing out from each terminal leaf in a tree. It works well, and is pretty fast, but I couldn't figure
+    out how to avoid a matrix inversion which should supposedly be possible. Thus, np.linalg.inv makes a 
+    clunky showing with all it's associated problems.
+
+    NOTE: I would really like to think of a way to normalize these values as a % independence-like metric
+    
+    Input(s):
+    rooted_tree - a Bio.Phylo tree object that must be properly rooted for the numbers to make any sense
+
+    Output(s):
+    weights_dict - the weights dictionary with key:val pairs of Bio.Phylo clade objects for each terminal (keys)
+            and floats representing the weight (vals)
+    rooted_tree - the rooted_tree object because it's possible I have clipped zero branch length clades and/or
+            altered the length of the root clade
+    """
     assert rooted_tree.is_bifurcating()
+    assert rooted_tree.rooted
+
     zero_bl_clades = [term for term in rooted_tree.get_terminals() if term.branch_length==0.]
     if len(zero_bl_clades) != 0:
         rooted_tree = trim_zero_bls(rooted_tree)
         print('Found some zero branch length terminals and have removed them. Note that '
                 'returned tree will contain fewer taxa than original')
-    if rooted_tree.root.branch_length == None:
-        rooted_tree.root.branch_length = 0.
+    rooted_tree.root.branch_length = 0.
     initial_order = rooted_tree.get_terminals()
     initial_matrix = np.zeros((len(initial_order),len(initial_order)))
     ###Calling the recursive function
@@ -145,10 +163,19 @@ def ACL_adhock(rooted_tree):
     return weights_dict, rooted_tree
 
 def vcv_recursive(putative_root, vcv_matrix, finished):
-    '''
-    Requires a rooted tree (binary root) and that root length should be zero. Also believe some zero bl's will royally screw things up
-    during matrix inversion
-    '''
+    """
+    This is a real fast recursive functoin to calculate the variance co-variance matrix. 
+    
+    Input(s):
+    putative_root - the root node (and subsequently, other clades) from a Bio.Phylo tree object
+    vcv_matrix - the matrix which will store everything and needs to be n x n where n is the 
+            total number of terminals in the tree object where putative_root belongs
+    finished - just a list to keep track of everything for record keeping
+
+    Output(s):
+    vcv_marix
+    finished
+    """
     terminals = putative_root.get_terminals()
     if not set(terminals).issubset(set(finished)):
         vcv_matrix[len(finished):len(finished)+len(terminals), len(finished):len(finished)+len(terminals)] += putative_root.branch_length
@@ -157,48 +184,50 @@ def vcv_recursive(putative_root, vcv_matrix, finished):
             vcv_matrix, finished = vcv_recursive(putative_root.clades[1], vcv_matrix, finished)
     elif len(putative_root.clades) == 0:
         finished.append(putative_root)
-    else:
-        print("ERROR: APPEARS TO BE A NON-BINARY TREE. MATRIX GENERATION WILL PROBABLY FAIL")
     return vcv_matrix, finished
 
+def trim_zero_bls(my_tree):
+    """
+    Certain weighting schemes (specifically ACL) really do not like zero branch length terminals so this code trims them
+    kind of randomly. Am still not entirely sure why this must be the case.
 
+    Input(s):
+    my_tree - a Bio.Phylo tree object
 
+    Output(s):
+    my_tree - the same tree object with zero branch length terminals removed
+    """
+    zero_bls = [term for term in my_tree.get_terminals() if term.branch_length==0.]
+    while len(zero_bls)>0:
+        my_tree.prune(random.choice(zero_bls))
+        zero_bls = [term for term in my_tree.get_terminals() if term.branch_length==0.]
+    return my_tree
 
-#def get_vcv_recursive(vcv_matrix, initial_clade, finished=[]):
-#   '''This is an old function that shoudl probably be ignored for now'''
-#    if len(initial_clade) == 2:
-#        if not set(initial_clade[0].get_terminals()).issubset(set(finished)):
-#            clade = initial_clade[0]
-#            vcv_matrix[len(finished):len(finished)+len(clade.get_terminals()), len(finished):len(finished)+len(clade.get_terminals())] += clade.branch_length
-#            vcv_matrix, finished = get_vcv_recursive(vcv_matrix, clade, finished)
-#
-#        if not set(initial_clade[1].get_terminals()).issubset(set(finished)):
-#            clade = initial_clade[1]
-#            vcv_matrix[len(finished):len(finished)+len(clade.get_terminals()), len(finished):len(finished)+len(clade.get_terminals())] += clade.branch_length
-#            vcv_matrix, finished = get_vcv_recursive(vcv_matrix, clade, finished)
-#    elif len(initial_clade) == 0:
-#        finished.append(initial_clade)
-#
-#    else:
-#        print("ERROR: APPEARS TO BE A NON-BINARY TREE. MATRIX GENERATION WILL PROBABLY FAIL")
-#    return vcv_matrix, finished
-
-
-
-#############Henikoff and Henikoff weights
+###################
+#HH weights
+###################
 def HH_adhock(records_list):
     """
     This is a modified Henikoff and Henikoff algorithm that I developed to correct
     for gapped sequences. The entire thing proceeds as usual but instead of either
     ignoring gaps or treating them as a 21st character, I give all gaps a weight of
-    zero and furthermore I downweight each column of the alignment according to whatever
+    zero and therefore downweight each column of the alignment according to whatever
     it's weight *would* be times the number of ungapped positions/all positions.
 
     Essentially, if a column has a ton of gaps those gaps all get zero and also the
-    remaining characters get heavily downweighted as well.
+    remaining characters get heavily downweighted as well because, in my thinking, the
+    column is largely uninformative.
 
     Depending on the application, the normalization at the bottom is pretty critical and
     should be considered (divide all values by the max?, the mean?, etc.)
+    
+    Input(s):
+    records_list - a list of Bio.Seq objects
+
+    Output(s):
+    weights_dict - a dictionary of key:val pairs where key is the id (from record.id) 
+            of sequences and the value is a float of the weight assigned to said
+            sequence
     """
     seqs = np.array([list(record.seq) for record in records_list])
     ids = [record.id for record in records_list]
@@ -208,7 +237,9 @@ def HH_adhock(records_list):
     all_weights = []
     for i in seqs_T[:]:
         counter_dict = Counter(i)
-        ###Comment/delete the below line to treat gaps as a 21st character
+        #Comment/delete the below line to treat gaps as a 21st character
+        #However I leave them in there and then use them to downweight
+        #the entire column as described in the docs
         del counter_dict['-']
         
         r = len(counter_dict.keys())
@@ -230,13 +261,7 @@ def HH_adhock(records_list):
     all_weights = weights_T.T
     all_weights = np.sum(all_weights, axis=1)
     all_weights = all_weights/np.max(all_weights)
-    final_weights_dict = dict(zip(ids, all_weights))
-    return final_weights_dict
+    weights_dict = dict(zip(ids, all_weights))
+    return weights_dict
 
 
-def trim_zero_bls(my_tree):
-    zero_bls = [term for term in my_tree.get_terminals() if term.branch_length==0.]
-    while len(zero_bls)>0:
-        my_tree.prune(random.choice(zero_bls))
-        zero_bls = [term for term in my_tree.get_terminals() if term.branch_length==0.]
-    return my_tree
